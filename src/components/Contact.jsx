7204b235c7b7b734c95156a97a2a4dd5b8ac0c63
nyarbor1 <113961150+nyarbor1@users.noreply.github.com>
2024-01-14 01:49:31 -0500

    initialized website

---
+++ src/components/Contact.jsx
@@ -0,0 +1,42 @@
+import oakpark from '../imgs/oakpark.png';
+import ContactIcon from '../imgs/contact.jpg';
+import location from '../imgs/location.png';
+
+const Contact = () => {
+    return (
+        <section className='contact'>
+            <h1 className='page-header'>Contact Us</h1>
+            <div className='container'>
+                <div className='column'>
+                    <div className='row'>
+                        <h5>Phone</h5>
+                        <h4>919-802-6142</h4>
+                    </div>
+                    <div className='row'>
+                        <h5>Address</h5>
+                        <h4>5009 Weaver Dr, Raleigh, NC 27612</h4>
+                    </div>
+                    <div className='row'>
+                        <h5>Service Areas</h5>
+                        <ul>
+                            <li>Cary</li>
+                            <li>Raleigh</li>
+                            <li>Morrisville</li>
+                            <li>Durham</li>
+                            <li>Apex</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className='column'>
+                    <img src={ContactIcon} alt="The whole YardLand team posing for a photo" />
+                </div>
+            </div>
+            <div className='img-container'>
+                <h2>Located here in Oak Park, Raleigh</h2>
+                <img src={location} alt="Map of Oak Park" />
+            </div>
+        </section>
+    );
+}
+
+export default Contact;
---
+++ src/components/Services.jsx
@@ -0,0 +1,63 @@
+const Services = () => {
+    return (
+        <section className='services'>
+            <h1 className='page-header'>Services</h1>
+            <div className='service-container'>
+                <div className='service'>
+                    <div className='column leafblowing'>
+                        <h2>Leaf Blowing</h2>
+                    </div>
+                    <div className='column'>
+                        <p>You really wanna look for that rake again? Let the professionals take care of it instead
+                        </p>
+                    </div>
+                </div>
+                <div className='service'>
+                    <div className='column mulch'>
+                        <h2>Mulch</h2>
+                    </div>
+                    <div className='column'>
+                        <p>$65 per cubic yard of mulch and we will cover everthing from delivery to installation.</p>
+                    </div>
+                </div>
+                <div className='service'>
+                    <div className='column lawncare'>
+                        <h2>Lawn Care</h2>
+                    </div>
+                    <div className='column'>
+                        <p>We offer a standard mow, blow, edging services or any combination of the three.</p>
+                    </div>
+                </div>
+                <div className='service'>
+                    <div className='column bushtrimming'>
+                        <h2>Bush Trimming</h2>
+                    </div>
+                    <div className='column'>
+                        <p>Our guys have a lot of experience trimming bushes and will take excellent care of yours.</p>
+                    </div>
+                </div>
+
+                <div className='service'>
+                    <div className='column clearout'>
+                        <h2>Clearouts</h2>
+                    </div>
+                    <div className='column'>
+                        <p>Turn your overgrown areas into new space you couldn't have imagined instantly!</p>
+                    </div>
+                </div>
+
+                <div className='service'>
+                    <div className='column haul'>
+                        <h2>Hauls</h2>
+                    </div>
+                    <div className='column'>
+                        <p>If you already have a pile of yardwaste or trash and need it removed ASAP, we can handle that right away for you!</p>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    );
+}
+
+export default Services;
